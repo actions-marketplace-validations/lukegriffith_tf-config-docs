2d4701bd82d0643d698dfa904caf0142f65f29f7
lukemgriffith <lukemgriffith@gmail.com>
2022-06-05 19:51:01 +0100

    bumbling about with react

--- www/app/src/App.js
+++ www/app/src/App.js
@@ -1,25 +1,121 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import { useTable } from 'react-table'
+
+
+
+
+
 
 function App() {
+
+  const [ModuleIndex, Modules] = useState([])
+
+  const request = async () => {
+    const index = await fetch('data/index')
+        .then(response => response.json())
+        .then(res => {
+          console.log(Reflect.ownKeys(res))
+          Reflect.ownKeys(res).forEach(key => { 
+              console.log(key)
+              fetch(`data/${key}`)
+                  .then(response => response.json())
+                  .then(data => console.log(data))
+        })
+    })
+  }
+
+  useEffect(() => {
+    request()
+  }, [])
+
+  const data = React.useMemo(
+    () => [
+      {
+        col1: 'Hello',
+        col2: 'World',
+      },
+      {
+        col1: 'react-table',
+        col2: 'rocks',
+      },
+      {
+        col1: 'whatever',
+        col2: 'you want',
+      },
+    ],
+    []
+  )
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Column 1',
+        accessor: 'col1', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Column 2',
+        accessor: 'col2',
+      },
+    ],
+    []
+  )
+
+  const tableInstance = useTable({ columns, data })
+ 
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.111
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // apply the table props
+    <table {...getTableProps()}>
+      <thead>
+        {// Loop over the header rows
+        headerGroups.map(headerGroup => (
+          // Apply the header row props
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {// Loop over the headers in each row
+            headerGroup.headers.map(column => (
+              // Apply the header cell props
+              <th {...column.getHeaderProps()}>
+                {// Render the header
+                column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      {/* Apply the table body props */}
+      <tbody {...getTableBodyProps()}>
+        {// Loop over the table rows
+        rows.map(row => {
+          // Prepare the row for display
+          prepareRow(row)
+          return (
+            // Apply the row props
+            <tr {...row.getRowProps()}>
+              {// Loop over the rows cells
+              row.cells.map(cell => {
+                // Apply the cell props
+                return (
+                  <td {...cell.getCellProps()}>
+                    {// Render the cell contents
+                    cell.render('Cell')}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
 
 export default App;
