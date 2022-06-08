e6ca5b5467c7ad09954af123023593748dd8ed39
Luke Griffith <lukemgriffith@gmail.com>
2022-06-09 00:07:16 +0100

    Adding changes for 0.0.5-alpha

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-/tf-config-docs --help
+modPath=$1
+outputPath=${2:-./docs}
+recurse=${3:-"false"}
+
+recruseArg=''
+
+if [[ "$recurse" = "true" ]]; then
+    recurseArg='-recurse'
+fi
+
+/tf-config-docs -modulePath $modPath -outputPath $outputPath $recurseArg
+
