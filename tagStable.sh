9bf6426478d0e87cfd45e1952ca6f64fe10ff45f
Luke Griffith <lukemgriffith@gmail.com>
2022-06-21 23:37:55 +0100

    making tag script better

--- tagStable.sh
+++ tagStable.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+set -e
+git fetch --tags
 TAG=$(git describe --tags)
-docker pull ghcr.io/lukegriffith/tf-config-docs:$TAG
+docker pull ghcr.io/lukegriffith/tf-config-docs:$TAG || echo "Container for $TAG might not be built."
 docker tag ghcr.io/lukegriffith/tf-config-docs:$TAG \
     ghcr.io/lukegriffith/tf-config-docs:stable
 docker push ghcr.io/lukegriffith/tf-config-docs:stable
