a37667b1883510b495c960ce037eee5b213e9a8c
Luke Griffith <lukemgriffith@gmail.com>
2022-06-21 22:05:55 +0100

    updating entry point with load env config

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,17 @@ set -x
 modPath=${1:-.}
 outputPath=${2:-./docs}
 recurse=${3:-"false"}
+loadEnvName=${4:-"false"}
 
 recruseArg=''
 
 if [[ "$recurse" = "true" ]]; then
     recurseArg='-recurse'
+fi
+
+if [[ "$loadEnvName" = "true" ]]; then
+    [ -z "$WORKDIR_FRIENDLY_NAME" ] && echo "laodEnvName provided but no env var found." && exit 1
+    loadEnvArg='-loadEnvName'
 fi
 
 git config --global --add safe.directory /github/workspace
@@ -19,7 +25,7 @@ cd /github/workspace
 
 mkdir $outputPath || rm -rf $outputPath/*
 cp -r /build/* $outputPath
-tf-config-docs -modulePath $modPath -outputPath $outputPath $recurseArg
+tf-config-docs -modulePath $modPath -outputPath $outputPath $recurseArg $loadEnvArg
 
 git checkout -b docs
 git add $outputPath
