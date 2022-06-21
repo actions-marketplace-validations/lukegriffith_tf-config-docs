3e4fc6ecdcb684772f31adc276be750609132bf6
Luke Griffith <lukemgriffith@gmail.com>
2022-06-21 23:17:55 +0100

    Updating getName to correctly fetch directory friendly name

--- app/src/lib/pathTools.js
+++ app/src/lib/pathTools.js
@@ -1,15 +1,17 @@
 const pathTools = {
     getName: (module, data) => {
         var moduleName = module
-
+        console.log("module", module)
         var directory = data.Config.Directories.filter(obj => {
             return obj.Path === module
         })
+        console.log("directory", directory)
 
         if (directory.length > 0 && 'FriendlyName' in directory[0]) {
-            moduleName = directory.FriendlyName
+            console.log(directory[0])
+            moduleName = directory[0].FriendlyName
         }
-
+        console.log("mdoule name", moduleName)
         const arr = moduleName.split("/")
         const [lastItem] = arr.slice(-1)
         return lastItem
