fe3f0fed07d71cac350b0173edbef112329a95f5
Luke Griffith <lukemgriffith@gmail.com>
2022-06-22 19:51:51 +0100

    refactoring

--- app/src/components/Module.js
+++ app/src/components/Module.js
@@ -4,51 +4,80 @@ import pathTools from '../lib/pathTools'
 import './Module.css'
 
 
-class Module extends React.Component {
-    render(props) {
-        return ModuleImpl(this.props)
-    }
-}
 
 
-function ModuleImpl(props) {
 
-    function getModule() {
-        var selectedModule
-        var moduleData
-        props.data.Modules.map(({ Module, Hash, Root, TfModule }) => {
-            if (Hash === props.module) {
-                selectedModule = pathTools.getName(Module, props.data)
-                moduleData = TfModule
-            }
-            return null
-        });
+function Module(props) {
 
-        
 
-        return (
-            <div>
-                <p>Name: {selectedModule}</p>
-                <p>{moduleData.path}</p>
-                <pre>
-                    <code>
-                        {JSON.stringify(moduleData, null, 2)}
-                    </code>
-                </pre>
-            </div>
-        )
-    }
-    function getModuleDiv() {
+    function ModuleHeader() {
         return (
             <div>
                 <h1>Module</h1>
                 <p>Module Hash: {props.module}</p>
-                {props.data && getModule()}
             </div>
+
+        )
+    }
+    
+    function ModuleStats() {
+    
+    }
+    
+
+    function ModuleContent() {
+
+        function SelectedModule(props) {
+            console.log(props)
+            return (
+                <div>
+                <p>Name: {props.selectedModule}</p>
+                <p>{props.moduleData.path}</p>
+                <pre>
+                    <code>
+                        {JSON.stringify(props.moduleData, null, 2)}
+                    </code>
+                </pre>
+            </div>
+            )
+        }
+
+        var selectedModule, moduleData = GetModule()
+
+        return (
+            <div>            
+                <SelectedModule selectedModule={selectedModule} moduleData={moduleData} /> 
+            </div>
+
         )
     }
 
-    var moduleNotNull = props.module != null
+    function GetModule() {
+        var moduleData, selectedModule
+        props.data.Modules.map(({ Module, Hash, Root, TfModule }) => {
+            if (Hash === props.module) {
+                selectedModule = pathTools.getName(Module, props.data)
+                moduleData = TfModule
+                return (selectedModule, moduleData)
+            }
+        });
+        return selectedModule, moduleData
+    }
+        
+
+
+    function getModuleDiv() {
+        return (
+            <div>
+                <ModuleHeader />
+                <ModuleStats />
+                <ModuleContent />
+            </div>
+
+        )
+    }
+
+    var moduleNotNull = props.module != null && props.data != null
 
     return (
         <div className="Module">
@@ -60,5 +89,9 @@ function ModuleImpl(props) {
 
 
 }
+
+
+
+
 
 export default Module
