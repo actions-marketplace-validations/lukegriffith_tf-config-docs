0eb414a5c2b55e25b8b228379eb32c438d561273
Luke Griffith <lukemgriffith@gmail.com>
2022-06-22 20:58:18 +0100

    various changes improving module.

--- app/src/components/Module.js
+++ app/src/components/Module.js
@@ -3,93 +3,59 @@ import React from 'react'
 import pathTools from '../lib/pathTools'
 import './Module.css'
 
-
-
-
-
 function Module(props) {
-
-
-    function ModuleHeader() {
-        return (
-            <div>
-                <h1>Module</h1>
-                <p>Module Hash: {props.module}</p>
-            </div>
-
-        )
-    }
-    
-    function ModuleStats() {
-    
-    }
-    
-
-    function ModuleContent() {
-
-        function SelectedModule(props) {
-            console.log(props)
-            return (
-                <div>
-                <p>Name: {props.selectedModule}</p>
-                <p>{props.moduleData.path}</p>
-                <pre>
-                    <code>
-                        {JSON.stringify(props.moduleData, null, 2)}
-                    </code>
-                </pre>
-            </div>
-            )
-        }
-
-        var selectedModule, moduleData = GetModule()
-
-        return (
-            <div>            
-                <SelectedModule selectedModule={selectedModule} moduleData={moduleData} /> 
-            </div>
-
-        )
-    }
-
+    console.log(1)
     function GetModule() {
         var moduleData, selectedModule
         props.data.Modules.map(({ Module, Hash, Root, TfModule }) => {
             if (Hash === props.module) {
                 selectedModule = pathTools.getName(Module, props.data)
                 moduleData = TfModule
-                return (selectedModule, moduleData)
             }
         });
-        return selectedModule, moduleData
+
+        return {
+            selectedModule: selectedModule,
+            moduleData: moduleData
+        }
     }
-        
-
-
-    function getModuleDiv() {
-        return (
-            <div>
-                <ModuleHeader />
-                <ModuleStats />
-                <ModuleContent />
-            </div>
-
-        )
-    }
-
-    var moduleNotNull = props.module != null && props.data != null
+    var moduleContext = GetModule()
+    const [showJson, setShowJson] = React.useState(false)
+    const jsonOnClick = () => setShowJson(!showJson)
 
     return (
         <div className="Module">
-            {moduleNotNull && getModuleDiv()}
+            <div className="moduleHeader">
+                <h1>Module</h1>
+                <p>Module Hash: {props.module}</p>
+                <p>Name: {moduleContext.selectedModule}</p>
+                <p>Path: {moduleContext.moduleData.path}</p>
+            </div>
+            <ModuleStats moduleContext={moduleContext} libraryData={props.data}/>        
+            <div className="moduleJson">
+                <button onClick={jsonOnClick}>json</button>
+                {
+                    showJson && <pre>
+                        <code>
+                            {JSON.stringify(moduleContext.moduleData, null, 2)}
+                        </code>
+                    </pre>
+                }
+
+            </div>
         </div>
+
     )
-
-
-
-
 }
 
+function ModuleStats(props) {
+    return (
+        <div className="statsGrid">
+            <h1>Stats</h1>
+            <p>TBA</p>
+        </div>
+    )
+}
 
 
 
