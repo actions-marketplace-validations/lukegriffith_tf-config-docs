e731e5c9e96bdaa2314690e7c3c0eb8dac256e5a
Luke Griffith <lukemgriffith@gmail.com>
2022-06-22 19:03:44 +0100

    Adding initial class wrapper.

--- app/src/components/Module.js
+++ app/src/components/Module.js
@@ -1,23 +1,35 @@
-import { useSearchParams } from "react-router-dom";
+//import { useSearchParams } from "react-router-dom";
 import React from 'react'
 import pathTools from '../lib/pathTools'
 import './Module.css'
 
-function Module(props) {
+
+class Module extends React.Component {
+    render(props) {
+        return ModuleImpl(this.props)
+    }
+}
+
+
+function ModuleImpl(props) {
 
     function getModule() {
         var selectedModule
         var moduleData
         props.data.Modules.map(({ Module, Hash, Root, TfModule }) => {
-            if (Hash == props.module) {
+            if (Hash === props.module) {
                 selectedModule = pathTools.getName(Module, props.data)
                 moduleData = TfModule
             }
+            return null
         });
+
+        
 
         return (
             <div>
                 <p>Name: {selectedModule}</p>
+                <p>{moduleData.path}</p>
                 <pre>
                     <code>
                         {JSON.stringify(moduleData, null, 2)}
